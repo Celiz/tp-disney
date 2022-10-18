5002f4fe0ec290a85f6461ac2265bff221e8b257
Celiz <maticelizramos@gmail.com>
2022-10-17 23:19:47 -0300

    Page add: password

--- pages/password.js
+++ pages/password.js
@@ -19,13 +19,13 @@ export default function Password() {
             <Navbar />
             <main className="main login">
                 <section className="login__section">
-                    <h3 className="h3">Restablecer contraseña</h3>
+                    <h3 className="h3">Iniciar sesión | Disney+</h3>
                     <p className="p">
                         Ingresa tu correo electrónico y te enviaremos un enlace para restablecer tu contraseña.
                     </p>
                     <input aria-invalid="false" aria-label="Escribe el correo electrónico que te gustaría usar para Disney Plus." aria-describedby="email__error" data-testid="" display="inline" id="email" maxlength="" name="email" placeholder="Correo electrónico" type="email" data-gv2elementkey="email" data-gv2interactionkey="email" className="form" />
                     <button aria-label="Aceptar y continuar" data-testid="login-continue-button" role="button" kind="primary" name="dssLoginSubmit" value="submit" className="btn" id="" type="submit" data-gv2elementkey="agree_&amp;_continue" data-gv2interactionkey="agree_&amp;_continue" data-gv2-interaction-bound="true">CONTINUAR</button>
-                    <section className="login__section--subscribe"><button>Olvidaste rty</button></section>
+                    <section className="login__section--subscribe"><button>¿Olvidaste la contraseña?</button></section>
                 </section>
             </main>
             <Footer />
