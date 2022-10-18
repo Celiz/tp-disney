35548e05aca9b942b1727f110658215387ee2908
Celiz <maticelizramos@gmail.com>
2022-10-17 23:10:30 -0300

    update!!

--- pages/login.js
+++ pages/login.js
@@ -18,12 +18,12 @@ export default function Login() {
       <Navbar />
       <main className="main login">
         <section className="login__section">
-          <h3 classname="h3">Inicia sesión con tu correo</h3>
-          <p classname="p">
+          <h3 className="h3">Inicia sesión con tu correo</h3>
+          <p className="p">
             Debes usar este correo electrónico y contraseña para iniciar sesión en tu cuenta de Disney+ y ver tus series y películas favoritas.
           </p>
-          <input aria-invalid="false" aria-label="Escribe el correo electrónico que te gustaría usar para Disney Plus." aria-describedby="email__error" data-testid="" display="inline" id="email" maxlength="" name="email" placeholder="Correo electrónico" type="email" data-gv2elementkey="email" data-gv2interactionkey="email" classname="form" value="" />
-          <button aria-label="Aceptar y continuar" data-testid="login-continue-button" role="button" kind="primary" name="dssLoginSubmit" value="submit" classname="btn" id="" type="submit" data-gv2elementkey="agree_&amp;_continue" data-gv2interactionkey="agree_&amp;_continue" data-gv2-interaction-bound="true">CONTINUAR</button>
+          <input aria-invalid="false" aria-label="Escribe el correo electrónico que te gustaría usar para Disney Plus." aria-describedby="email__error" data-testid="" display="inline" id="email" maxlength="" name="email" placeholder="Correo electrónico" type="email" data-gv2elementkey="email" data-gv2interactionkey="email" className="form" />
+          <button aria-label="Aceptar y continuar" data-testid="login-continue-button" role="button" kind="primary" name="dssLoginSubmit" value="submit" className="btn" id="" type="submit" data-gv2elementkey="agree_&amp;_continue" data-gv2interactionkey="agree_&amp;_continue" data-gv2-interaction-bound="true">CONTINUAR</button>
           <section className="login__section--subscribe"><p class="p">¿Primera vez en Disney+?</p> <button>Suscribirse</button></section>
         </section>
       </main>
